7224a1628582407d7ff692b58d2e83b1018f5e70
Uchenna Nwaigwe <hermessuffice@gmail.com>
2025-06-08 12:30:20 +0100

    End of Building a Walking Skeleton for API - 2

---
+++ Persistence/Migrations/20250605193014_UpdateActivity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateActivity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longtitude",
+                table: "Activities",
+                newName: "Longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "Latitiude",
+                table: "Activities",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longitude",
+                table: "Activities",
+                newName: "Longtitude");
+
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "Activities",
+                newName: "Latitiude");
+        }
+    }
+}
