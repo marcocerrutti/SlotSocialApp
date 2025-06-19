32edaef3a96ab33f6308e914a9c738ddb16a8af9
Uchenna Nwaigwe <hermessuffice@gmail.com>
2025-06-19 01:57:10 +0100

    End of building a walking skeleton partII React  - 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
